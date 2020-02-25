4e3ff164642c18028a3bf0156c4adc984b0a4c35
Jônatas da Silva <jonatas.silva.roots@gmail.com>
2020-02-24 22:56:12 -0300

    meu segundo layout

---
+++ js/index.js
@@ -0,0 +1,35 @@
+var checkbox = document.querySelector('input[name=contraste]');
+
+checkbox.addEventListener('change', function() {
+    if (this.checked) {
+        document.documentElement.setAttribute('data-theme', 'light');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+});
+
+const navSlide = () => {
+    // Elementos
+    const nav = document.querySelector('.navegar-menu'); //ul do documento
+    const burger = document.querySelector('.burger'); //botão burger
+    const links = document.querySelectorAll('.navegar-menu li'); //coleta todas as lis para aplicar animação, pode ser removido
+
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        // Animate Links
+        links.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navegarFade 0.5s ease forwards ${index /7 + 0.45}s`;
+            }
+        });
+
+        // Burger Animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
